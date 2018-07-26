4d13b48a5e749fc5fe1f89410d28929ee726810b
apple <apple@gmail.com>
2018-07-26 23:31:28 +0530

    gradle pro

---
+++ src/main/java/com/example/super/SuperApplication.java
@@ -0,0 +1,12 @@
+package com.example.super;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SuperApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SuperApplication.class, args);
+	}
+}
